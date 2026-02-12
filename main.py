3ced8307e81da0f483e0bdc598e981dc5d346f09
Zienab297 <zienabelsheikh@gmail.com>
2026-02-12 21:19:39 +0200

    backend

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+from routes.base import base_router
 app = FastAPI()
 
-@app.get("/welcome")
-
-def welcome():
-    return {
-        "message": "Welcome to FastAPI!"
-        }
-
+app.include_router(base_router)
